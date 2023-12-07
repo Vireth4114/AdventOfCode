1c6507e8ab054219851767b9fc481adf55faa691
aubry224 <aubry224@unicaen.fr>
2023-12-07 11:51:31 +0100

    better day 7

--- 7.py
+++ 7.py
@@ -3,15 +3,12 @@ lines = useful.get_lines("7.txt")
 
 def silverstar():
     listhands = [[], [], [], [], [], [], []]
+    order = "23456789TJQKA"
     dict_bid = dict()
     for line in lines:
-        hand = line.split(' ')[0].replace('A', 'E').replace('K', 'D').replace('Q', 'C').replace('J', 'B').replace('T', 'A')
+        hand = line.split(' ')[0]
         dict_bid[hand] = int(line.split(' ')[1])
-        countcard = []
-
-        for c in (set(hand)):
-            countcard.append(hand.count(c))
-        sortedvalues = sorted(countcard, reverse=True)[:2]
+        sortedvalues = sorted([hand.count(card) for card in set(hand)], reverse=True)[:2]
 
         for idx, value in enumerate(([5], [4, 1], [3, 2], [3, 1], [2, 2], [2, 1], [1, 1])):
             if sortedvalues == value:
@@ -20,27 +17,23 @@ def silverstar():
 
     truelist = []
     for entry in reversed(listhands):
-        truelist += sorted(entry)
+        truelist += sorted(entry, key=lambda entry: [order.index(card) for card in entry])
     
     return sum((index+1)*dict_bid[entry] for index, entry in enumerate(truelist))
 
 def goldstar():
     listhands = [[], [], [], [], [], [], []]
+    order = "J23456789TQKA"
     dict_bid = dict()
     for line in lines:
-        hand = line.split(' ')[0].replace('A', 'D').replace('K', 'C').replace('Q', 'B').replace('T', 'A').replace('J', '1')
+        hand = line.split(' ')[0]
         dict_bid[hand] = int(line.split(' ')[1])
-        countcard = []
-
-        for c in (set(hand)):
-            if c != '1':
-                countcard.append(hand.count(c))
-        sortedvalues = sorted(countcard, reverse=True)[:2]
+        sortedvalues = sorted([hand.count(card) for card in set(hand) if card != 'J'], reverse=True)[:2]
 
         if len(sortedvalues) == 0:
             sortedvalues = [5]
         else:
-            sortedvalues[0] += hand.count('1')
+            sortedvalues[0] += hand.count('J')
 
         for idx, value in enumerate(([5], [4, 1], [3, 2], [3, 1], [2, 2], [2, 1], [1, 1])):
             if sortedvalues == value:
@@ -49,7 +42,7 @@ def goldstar():
 
     truelist = []
     for entry in reversed(listhands):
-        truelist += sorted(entry)
+        truelist += sorted(entry, key=lambda entry: [order.index(card) for card in entry])
     
     return sum((index+1)*dict_bid[entry] for index, entry in enumerate(truelist))
 
