3a71ad1e6c96f5e4cbaf6714ca08e7426eb12f5b
aubry224 <aubry224@unicaen.fr>
2023-12-16 15:53:59 +0100

    Better day 16

--- 16.py
+++ 16.py
@@ -4,53 +4,14 @@ from time import time
 
 lines = useful.get_list_lines("16.txt")
 
-def silverstar():
-    set_all = set()
-    cache = set()
-    currents = set([(0, -1, (0, 1))])
-    while currents != set():
-        for current in deepcopy(currents):
-            dir = current[2]
-            currents.remove(current)
-            set_all.add(tuple((current[0], current[1])))
-            cache.add(current)
-            if not (0 <= current[0]+dir[0] < len(lines) and 0 <= current[1]+dir[1] < len(lines[0])):
-                break
-
-            thing_to_add = []
-            mirror = lines[current[0]+dir[0]][current[1]+dir[1]]
-            match mirror:
-                case '.':
-                    thing_to_add.append(dir)
-                case '/':
-                    thing_to_add.append((-1*dir[1], -1*dir[0]))
-                case '\\':
-                    thing_to_add.append((dir[1], dir[0]))
-                case '|':
-                    if dir[0] == 0:
-                        thing_to_add.append((1, 0))
-                        thing_to_add.append((-1, 0))
-                    else:
-                        thing_to_add.append(dir)
-                case '-':
-                    if dir[1] == 0:
-                        thing_to_add.append((0, 1))
-                        thing_to_add.append((0, -1))
-                    else:
-                        thing_to_add.append(dir)
-            
-            for thing in thing_to_add:
-                addition = tuple((current[0]+dir[0], current[1]+dir[1], thing))
-                if addition not in cache:
-                    currents.add(addition)
-    set_all.remove((0, -1))
-    return len(set_all)
-
-def goldstar():
-    beginning = [tuple([c, -1, (0, 1)]) for c in range(len(lines))]+\
-                [tuple([c, len(lines[0]), (0, -1)]) for c in range(len(lines))]+\
-                [tuple([-1, c, (1, 0)]) for c in range(len(lines[0]))]+\
-                [tuple([len(lines), c, (-1, 0)]) for c in range(len(lines[0]))]
+def star(mode):
+    if mode == 'gold':
+        beginning = [tuple([c, -1, (0, 1)]) for c in range(len(lines))]+\
+                    [tuple([c, len(lines[0]), (0, -1)]) for c in range(len(lines))]+\
+                    [tuple([-1, c, (1, 0)]) for c in range(len(lines[0]))]+\
+                    [tuple([len(lines), c, (-1, 0)]) for c in range(len(lines[0]))]
+    else:
+        beginning = [(0, -1, (0, 1))]
 
     max_result = -1
     for start in beginning:
@@ -58,7 +19,7 @@ def goldstar():
         cache = set()
         currents = set([start])
         while currents != set():
-            for current in deepcopy(currents):
+            for current in currents.copy():
                 dir = current[2]
                 currents.remove(current)
                 set_all.add(tuple((current[0], current[1])))
@@ -101,7 +62,7 @@ def goldstar():
 
 if __name__ == '__main__':
     t1 = time()
-    print(f"Silver star: {silverstar()}")
-    print(f"Gold star: {goldstar()}")
+    print(f"Silver star: {star('silver')}")
+    print(f"Gold star: {star('gold')}")
     t2 = time()
     print(f'took {round((t2-t1)*1000)} ms')
